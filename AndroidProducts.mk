e60a8454320b49f29f7ea8c5f5f1026900107e1a
Wim N <wimaen@gmail.com>
2019-01-28 17:29:14 +0100

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -13,5 +13,5 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_bacon.mk
+# PRODUCT_MAKEFILES := \
+#    $(LOCAL_DIR)/lineage_bacon.mk
