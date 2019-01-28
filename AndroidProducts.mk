ce6500ff899aecb0950df708b5f7566250283035
Wim N <wimaen@gmail.com>
2019-01-28 20:41:41 +0100

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -13,5 +13,5 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# PRODUCT_MAKEFILES := \
-#    $(LOCAL_DIR)/validus_bacon.mk
+PRODUCT_MAKEFILES := \
+   $(LOCAL_DIR)/validus_bacon.mk
